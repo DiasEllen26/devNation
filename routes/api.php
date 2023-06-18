1709b1148e264b9e0a0ea4a0201ef9215a238da9
DiasEllen26 <ellen.c.s.dias@outlook.com.br>
2023-06-18 13:23:11 -0300

    Iniciando configurações das rotas

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProgrammerController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//rota principal
+Route::get('/programadores', [ProgrammerController::class, 'index']);
+//Enviar para o banco de dados
+Route::post('/programadores', [ProgrammerController::class, 'store']);
+//Trás informações do banco de dados
+Route::get('programadores', [ProgrammerController::class], 'show');
