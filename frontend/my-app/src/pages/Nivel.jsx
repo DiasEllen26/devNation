ce9c53ca6b1010ec8bc77dd87364d0a35fd6d1dd
DiasEllen26 <ellen.c.s.dias@outlook.com.br>
2023-06-22 20:12:12 -0300

    Iniciando tela Home

---
+++ frontend/my-app/src/pages/Nivel.jsx
@@ -0,0 +1,8 @@
+// NiveisPage.js
+import React from 'react';
+
+function Nivel() {
+  return <h2>Página Níveis</h2>;
+}
+
+export default Nivel;
---
+++ frontend/my-app/src/pages/Programador.jsx
@@ -0,0 +1,8 @@
+// NiveisPage.js
+import React from 'react';
+
+function Programador() {
+  return <h2>Página Programador</h2>;
+}
+
+export default Programador;
